70f106996b67bc4a047c7cccbeca0e1f817874c6
Luis Henrique <luisenriklh1177027@gmail.com>
2020-10-23 22:29:23 -0700

    Desenvolvimento do back-end

---
+++ src/server.js
@@ -0,0 +1,16 @@
+// importar dependência
+const express = require('express');
+const path = require('path');
+
+// iniciando o express
+const server = express()
+server
+// utilizando os arquivos estáticos
+.use(express.static('public'))
+// criar uma rota
+.get('/', (request, response) => {
+  return response.sendFile(path.join(__dirname, 'views', 'index.html'));
+})
+
+// ligar o servidor
+server.listen(5500)
