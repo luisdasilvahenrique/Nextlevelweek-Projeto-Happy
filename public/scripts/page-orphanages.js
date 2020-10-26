af479679be1ce35c39d61367e2ae23f140e3ebfc
Luis Henrique <luisenriklh1177027@gmail.com>
2020-10-25 17:33:13 -0700

    Criação da Template Engine

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -7,7 +7,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
 //create icon
 const icon = L.icon({ 
-  iconUrl: "./public/images/map-marker.svg",
+  iconUrl: "/images/map-marker.svg",
   iconSize: [58, 68],
   iconAnchor: [29, 68],
   popup: [170, 5]
@@ -19,7 +19,7 @@ const popup = L.popup({
   className: 'map-popup',
   minWight: 240,
   minHeight: 240
-}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg"> </a>')
+}).setContent('Lar das meninas <a href="/orphanage?id=1" class="choose-orphanage"> <img src="/images/arrow-white.svg"> </a>')
 
 //create and add marker
 L
