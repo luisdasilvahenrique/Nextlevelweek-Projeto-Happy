ae9db587bc92914fea07168fe018780f4c329271
Luis Henrique <luisenriklh1177027@gmail.com>
2020-10-29 21:12:29 -0700

    Criação do banco de dados da aplicação

--- src/database/fakedata.js
+++ src/database/fakedata.js
@@ -1,9 +1,9 @@
 module.exports = [
   {
       id: 1,
-      name: "Lar das meninas",
       lat: "-27.222633",
       lng: "-49.6455874",
+      name: "Lar das meninas",
       description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
       images: [
         "https://images.unsplash.com/photo-1601564267675-0377e2501d4b?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
