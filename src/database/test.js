f91a3d88a98281a0fbae63c667b12f475bbb5003
Luis Henrique <luisenriklh1177027@gmail.com>
2020-11-10 08:13:33 -0300

    criação do banco de dados ficticio

--- src/database/test.js
+++ src/database/test.js
@@ -1,31 +1,23 @@
 const Database = require('./db');
+const saveOrphanage = require('./saveOrphanage');
 
 Database.then(async db => {
   // inserir dados na tabela
-  /*await db.run(`
-    INSERT INTO orphanages (
-      lat,
-      lng,
-      name,
-      about,
-      whatsapp,
-      images,
-      instructions,
-      opening_hours,
-      open_on_weekends
-    ) VALUES (
-      "-27.222633",
-      "-49.6555874",
-      "Lar dos meninos",
-      "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
-      "989848324"
+  /* await saveOrphanage(db, {
+    lat: "-27.222633",
+    lng: "-49.6555874",
+    name: "Lar dos meninos",
+    about: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+    whatsapp: "9999998899",
+    images: [
       "https://images.unsplash.com/photo-1601564267675-0377e2501d4b?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
-      "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
-      "Horário de visitas Das 18h até 8h",
-      "0"
 
-    );
-  `) */
+      "https://images.unsplash.com/photo-1598454444233-9dc334394ed3?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+    ].toString(),
+    instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+    opening_hours: "Horário de visitas Das 18h até 8h",
+    open_on_weekends: ""
+}) */
 
   // consultar dados da tabela
   const selectedOrphanages = await db.all("SELECT * FROM orphanages")
@@ -34,4 +26,8 @@ Database.then(async db => {
   // consultar somente 1 orfanato, pelo id
   const orphanage = await db.all('SELECT * FROM orphanages WHERE id = "2"')
   console.log(orphanage)
+
+  //deletar dado da tabela
+  //console.log(await db.run("DELETE FROM orphanages WHERE id='4'"))
+  //cconsole.log(await db.run("DELETE FROM orphanages WHERE id='5'"))
 })
